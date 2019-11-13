71c1c09fa752e98f7d71690753cb940204e8b364
JacksonCosta <36684135+JacksonCosta@users.noreply.github.com>
2019-11-13 17:04:46 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -13,22 +13,14 @@ require 'pry'
 require 'date'
 
 
-# Capybara.register_driver :selenium do |app|
-# 	Capybara::Poltergeist::Driver.new(app, js_errors: false, :default_max_wait_time => 120, :timeout => 120)
-# end
 
-
-  Capybara.register_driver :selenium do |app| 
-    Capybara::Poltergeist::Driver.new(app, { js_errors: false, timeout: 300, phantomjs_options: [ '--load-images=no', '--ignore-ssl-errors=yes', '--ssl-protocol=any']}) 
+Capybara.register_driver :selenium do |app| 
+    Capybara::Poltergeist::Driver.new(app, { js_errors: false, timeout: 300, phantomjs_options: [ '--debug=no', '--ssl-protocol=TLSv1', '--load-images=false', '--ignore-ssl-errors=yes', '--ssl-protocol=any', '--ignore-ssl-errors=true']}) 
 end
 
 Capybara.configure do |config|
     config.default_driver = :selenium
-        
-    
-
-  # config.app_host = 'https://des.quantumweb.com.br:4434/gofree/teste/site/'
-  config.app_host = 'https://des.quantumweb.com.br:4434/gofree/automatizado/site/'
+    config.app_host = 'https://des.quantumweb.com.br:4434/gofree/automatizado/site/'
 end
 
 #SitePrism.configure do |config|
