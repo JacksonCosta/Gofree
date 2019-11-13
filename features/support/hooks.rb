4088ab6f2c1a1369de38c73f32cc350c3aa6e886
JacksonCosta <36684135+JacksonCosta@users.noreply.github.com>
2019-11-13 11:44:01 -0300

    Update hooks.rb

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -40,6 +40,6 @@ After do |scenario|
     nome_cenario = scenario.name.tr(' ', '_').downcase!
     nome_cenario = nome_cenario.gsub(/([_@#!%()\-=;><,{}\~\[\]\.\/\?\"\*\^\$\+\-]+)/, '')
     screenshot = "logs/shots/#{nome_cenario}.png"
-    page.save_screenshot(screenshot)
-    embed(screenshot, 'image/png', 'Clique para ver a evidência!')
+    #page.save_screenshot(screenshot)
+    #embed(screenshot, 'image/png', 'Clique para ver a evidência!')
 end
