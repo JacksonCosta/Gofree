61c753c82367e21b165fb2638140920af6b361fb
JacksonCosta <36684135+JacksonCosta@users.noreply.github.com>
2019-11-20 16:19:17 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -13,19 +13,19 @@ require 'pry'
 require 'date'
 
 
-#Capybara.register_driver :selenium do |app|
-  #Capybara::Selenium::Driver.new(app, { js_errors: false, chrome_options: ['--headless', '--no-sandbox', '--disable-dev-shm-usage', '--remote-debugging-port=9222']})
-#end
-Capybara.register_driver :chrome_headless do |app|
-  options = ::Selenium::WebDriver::Chrome::Options.new
+Capybara.register_driver :selenium do |app|
+   Capybara::Selenium::Driver.new(app, { js_errors: false, chrome_options: ['--no-sandbox', '--disable-setuid-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']})
+end
+#Capybara.register_driver :chrome_headless do |app|
+ # options = ::Selenium::WebDriver::Chrome::Options.new
 
-  options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
+  #options.add_argument('--headless')
+  #options.add_argument('--no-sandbox')
+  #options.add_argument('--disable-dev-shm-usage')
   #options.add_argument('--window-size=1400,1400')
 
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-end
+  #Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+#end
 
 
 Capybara.configure do |config|
