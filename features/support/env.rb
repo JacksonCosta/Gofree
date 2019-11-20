78123831e97d00ec3e28ca5694781bdb12bcde15
JacksonCosta <36684135+JacksonCosta@users.noreply.github.com>
2019-11-20 16:36:27 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -14,7 +14,7 @@ require 'date'
 
 
 Capybara.register_driver :selenium do |app|
-   Capybara::Selenium::Driver.new(app, { js_errors: false, chrome_options: ['--nogui=True', '--disable-setuid-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']})
+   Capybara::Selenium::Driver.new(app, { js_errors: false, chrome_options: ['no-sandbox', '--disable-setuid-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']})
 end
 
 
