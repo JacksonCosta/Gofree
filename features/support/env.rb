b3899e5bf8ea4ef5098a0faadb75882bddab7c61
JacksonCosta <36684135+JacksonCosta@users.noreply.github.com>
2019-11-20 16:27:45 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,20 +16,10 @@ require 'date'
 Capybara.register_driver :selenium do |app|
    Capybara::Selenium::Driver.new(app, { js_errors: false, chrome_options: ['--no-sandbox', '--disable-setuid-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']})
 end
-#Capybara.register_driver :chrome_headless do |app|
- # options = ::Selenium::WebDriver::Chrome::Options.new
-
-  #options.add_argument('--headless')
-  #options.add_argument('--no-sandbox')
-  #options.add_argument('--disable-dev-shm-usage')
-  #options.add_argument('--window-size=1400,1400')
-
-  #Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
-#end
 
 
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
+    config.default_driver = :selenium_chrome_headless
 
     config.app_host = 'https://des.quantumweb.com.br:4434/gofree/automatizado/site/'
 end
