ed8ef33e8505bf245fbe8fa21555ef514f8741fb
JacksonCosta <36684135+JacksonCosta@users.noreply.github.com>
2019-11-22 10:01:02 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,14 +12,26 @@ require 'pry'
 
 require 'date'
 
-chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+Capybara.register_driver :chrome_headless do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
 
-chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1400,1400')
 
-Capybara.register_driver :selenium do |app|
-   Capybara::Selenium::Driver.new(app, { js_errors: false, chrome_options: ['no-sandbox']})
-   #, '--disable-setuid-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222'
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
+
+#/
+#chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+
+#chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+
+#Capybara.register_driver :selenium do |app|
+   #Capybara::Selenium::Driver.new(app, { js_errors: false, chrome_options: ['no-sandbox', '--disable-setuid-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']})
+   
+#end
 
 
 
