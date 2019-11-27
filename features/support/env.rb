91ae3c1112e3c824c861d429d741fecff48f43e0
JacksonCosta <36684135+JacksonCosta@users.noreply.github.com>
2019-11-27 10:40:38 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -17,9 +17,20 @@ require 'date'
 
 
 Capybara.register_driver :selenium do |app|
-    Capybara::Selenium::Driver.new(app, :browser => :chrome, chrome_options: ['--no-sandbox', '--disable-setuid-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222'])
-  
+    Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
+
+Capybara.register_driver :selenium_chrome do |app|
+   options = ::Selenium::WebDriver::Chrome::Options.new
+ 
+   options.add_argument('--headless')
+   options.add_argument('--no-sandbox')
+   options.add_argument('--disable-dev-shm-usage')
+   #options.add_argument('--window-size=1400,1400')
+ 
+   Capybara::Selenium::Driver.new(app, browser: :chrome_headless, options: options)
+   
+ end
 
 
 Capybara.configure do |config|
