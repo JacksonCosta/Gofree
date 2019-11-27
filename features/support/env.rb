95c29e4dfca8919fb6dc85b15857a782973fbd36
JacksonCosta <36684135+JacksonCosta@users.noreply.github.com>
2019-11-27 10:51:01 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -13,28 +13,38 @@ require 'pry'
 require 'date'
 
 
-
-
-
 Capybara.register_driver :selenium do |app|
     Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
 
-Capybara.register_driver :selenium_chrome do |app|
-   options = ::Selenium::WebDriver::Chrome::Options.new
+Capybara.register_driver :selenium do |app| 
+    Capybara::Poltergeist::Driver.new(app, js_errors: false)
+#, timeout: 300, phantomjs_options: [ '--debug=no', '--ssl-protocol=any', '--load-images=yes', '--ignore-ssl-errors=yes', '--output-encoding=utf8']}) 
+    
+#   }
+end
+
+
+
+#Capybara.register_driver :selenium do |app|
+    #Capybara::Selenium::Driver.new(app, :browser => :chrome)
+#end
+
+#Capybara.register_driver :selenium_chrome do |app|
+   #options = ::Selenium::WebDriver::Chrome::Options.new
  
-   options.add_argument('--headless')
-   options.add_argument('--no-sandbox')
-   options.add_argument('--disable-dev-shm-usage')
-   #options.add_argument('--window-size=1400,1400')
+   #options.add_argument('--headless')
+   #options.add_argument('--no-sandbox')
+   #options.add_argument('--disable-dev-shm-usage')
+   ##options.add_argument('--window-size=1400,1400')
  
-   Capybara::Selenium::Driver.new(app, browser: :chrome_headless, options: options)
+   #Capybara::Selenium::Driver.new(app, browser: :chrome_headless, options: options)
    
- end
+ #end
 
 
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome_headless
+    config.default_driver = :selenium
 
     
 
