f811d054c1b9b08ef602883245032614d481331f
JacksonCosta <36684135+JacksonCosta@users.noreply.github.com>
2019-11-27 09:12:44 -0300

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -12,31 +12,50 @@ require 'pry'
 
 require 'date'
 
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
+World Capybara::DSL
+#BROWSER = ENV['BROWSER']
 
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::chrome(
-    chromeOptions: { args: %w(headless no-sandbox) }
-  )
+Capybara.default_driver = :selenium_chrome
 
-#   Capybara::Selenium::Driver.new app,
-#     browser::chrome,
-#     #desired_capabilities: capabilities
-end
+#if BROWSER.eql?('remote')
+  Capybara.register_driver :selenium_chrome do |app|
+    Capybara::Selenium::Driver.new(app,
+      :browser => :remote,
+      :desired_capabilities => :chrome,
+      :url => "https://des.quantumweb.com.br:4434/gofree/automatizado/site/"
+    )
+  end
+#else
+  Capybara.register_driver :selenium_chrome do |app|
+    Capybara::Selenium::Driver.new(app, :browser => :chrome)
+  end
+#end
 
 
 
 
-Capybara.configure do |config|
-    config.default_driver = :selenium_chrome_headless
+#Capybara.register_driver :chrome do |app|
+  #Capybara::Selenium::Driver.new(app, browser: :chrome)
+#end
+
+#Capybara.register_driver :headless_chrome do |app|
+  #capabilities = Selenium::WebDriver::Remote::chrome(
+   # chromeOptions: { args: %w(headless no-sandbox) }
+  #)
+
+#end
+
+
+
+
+#Capybara.configure do |config|
+    #config.default_driver = :selenium_chrome_headless
 
     
 
   
-  config.app_host = 'https://des.quantumweb.com.br:4434/gofree/automatizado/site/'
-end
+  #config.app_host = 'https://des.quantumweb.com.br:4434/gofree/automatizado/site/'
+#end
 
 
 Capybara.default_max_wait_time = 10
