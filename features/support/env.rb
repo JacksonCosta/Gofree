7643109592dd191d25ca08bb2cdb840c2f748c04
JacksonCosta <36684135+JacksonCosta@users.noreply.github.com>
2020-05-20 16:17:13 -0300

    Testes

--- features/support/env.rb
+++ features/support/env.rb
@@ -20,21 +20,21 @@ require 'date'
 
 
 
-Capybara.register_driver :selenium do |app|
-    Capybara::Selenium::Driver.new(app, :browser => :chrome)
-end
+#Capybara.register_driver :selenium do |app|
+    #Capybara::Selenium::Driver.new(app, :browser => :chrome)
+#end
 
-Capybara.register_driver :selenium_chrome do |app|
-   options = ::Selenium::WebDriver::Chrome::Options.new
+#Capybara.register_driver :selenium_chrome do |app|
+   #options = ::Selenium::WebDriver::Chrome::Options.new
  
-   options.add_argument('--headless')
-   options.add_argument('--no-sandbox')
-   options.add_argument('--disable-dev-shm-usage')
+  # options.add_argument('--headless')
+   #options.add_argument('--no-sandbox')
+   #options.add_argument('--disable-dev-shm-usage')
    #options.add_argument('--window-size=1400,1400')
  
-   Capybara::Selenium::Driver.new(app, browser: :chrome_headless, options: options)
+   #Capybara::Selenium::Driver.new(app, browser: :chrome_headless, options: options)
    
- end
+ #end
 
 
 Capybara.configure do |config|
