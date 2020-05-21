69f31b4fff3377fc02c7e7b20d4e79c91ec487d4
JacksonCosta <36684135+JacksonCosta@users.noreply.github.com>
2020-05-21 10:53:44 -0300

    Atualização hooks

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -36,9 +36,8 @@ end
 
 # ===== Slavar imagens e nomea-las com os cenários =========
 After do |scenario|
-    nome_cenario = scenario.name.tr(' ', '_').downcase!
-    nome_cenario = nome_cenario.gsub(/([_@#!%()\-=;><,{}\~\[\]\.\/\?\"\*\^\$\+\-]+)/, '')
-    screenshot = "logs/shots/#{nome_cenario}.png"
-    page.save_screenshot(screenshot)
-    embed(screenshot, 'image/png', 'Clique para ver a evidência!')
+  screenshot = page.save_screenshot("log/screenshots/#{scenario.__id__}.png")
+  embed(screenshot, "image/png", "Screenshot")
 end
+
+
