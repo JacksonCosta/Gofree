b139ef004189036390497dd27313bbde644a1739
JacksonCosta <36684135+JacksonCosta@users.noreply.github.com>
2020-05-21 10:40:33 -0300

    Atualização

--- features/support/env.rb
+++ features/support/env.rb
@@ -13,38 +13,28 @@ require 'pry'
 require 'date'
 
 
-#Capybara.register_driver :selenium do |app| 
-    #Capybara::Poltergeist::Driver.new(app, { js_errors: false, timeout: 300, phantomjs_options: [ '--debug=no', '--ssl-protocol=any', '--load-images=yes', '--ignore-ssl-errors=yes', '--output-encoding=utf8']})
-
-#end
-
-
-
-#Capybara.register_driver :selenium do |app|
-    #Capybara::Selenium::Driver.new(app, :browser => :chrome)
-#end
-
-#Capybara.register_driver :selenium_chrome do |app|
-   #options = ::Selenium::WebDriver::Chrome::Options.new
- 
-  # options.add_argument('--headless')
-   #options.add_argument('--no-sandbox')
-   #options.add_argument('--disable-dev-shm-usage')
-   #options.add_argument('--window-size=1400,1400')
- 
-   #Capybara::Selenium::Driver.new(app, browser: :chrome_headless, options: options)
-   
- #end
-
-
-Capybara.configure do |config|
-    config.default_driver = :selenium_chrome_headless
-
-    
-
-  
-  config.app_host = 'https://des.quantumweb.com.br:4434/gofree/automatizado/site/'
+case ENV["BROWSER"]
+when "firefox"
+  @driver = :selenium
+when "chrome"
+  @driver = :selenium_chrome
+when "headless"
+  Capybara.register_driver :selenium_chrome_headless do |app|
+    chrome_options =  Selenium::WebDriver::Chrome::Options.new.tap do |options|
+      options.add_argument "--headless"
+      options.add_argument "--disable-gpu"
+      options.add_argument "--no-sandbox"
+      options.add_argument "--disable-site-isolation-trials"
+    end
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+  end
+  @driver = :selenium_chrome_headless
+else
+  puts "Invalid browser"
 end
 
-
-Capybara.default_max_wait_time = 10
+Capybara.configure do |config|
+  config.default_driver = @driver
+  
+  config.default_max_wait_time = 10
+end
